0f295c49d7568e837149c5a8e279f1ef6b08b288
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-06-07 16:08:48 +0200

    move compile flags to write_compile_info

--- src/cmdstan/write_compile_info.hpp
+++ src/cmdstan/write_compile_info.hpp
@@ -13,6 +13,19 @@ void write_compile_info(stan::callbacks::writer &writer,
     for (int i = 0; i < compile_info.size(); i++) {
        writer(compile_info[i]);
     }
+#ifdef STAN_THREADS
+    writer("STAN_THREADS=true");
+#endif
+#ifdef STAN_MPI
+    writer("STAN_MPI=true");
+#endif
+#ifdef STAN_OPENCL
+    writer("STAN_OPENCL=true");
+    std::stringstream msg;
+    msg << "OPENCL_PLATFORM_ID=" << OPENCL_PLATFORM_ID << "\n"
+        << "OPENCL_DEVICE_ID=" << OPENCL_DEVICE_ID;
+    writer(msg.str());
+#endif
 }
 }  // namespace cmdstan
 #endif
